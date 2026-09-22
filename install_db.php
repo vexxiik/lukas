e16094d47d9f47435b936d7a42a0edccb8964571
Lukas <lukas@terracomplexapartment.com>
2026-09-22 22:11:45 +0200

    Add install_db.php script for easy setup

---
+++ install_db.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/db.php';
+$sql = file_get_contents(__DIR__ . '/setup.sql');
+try {
+    $pdo->exec($sql);
+    echo "<h1>Struktura databaze byla uspesne vytvorena! Vse bezi na 155%!</h1>";
+    echo "<p><a href='/'>Zpet na web</a></p>";
+} catch(PDOException $e) {
+    echo "Chyba pri vytvareni tabulek: " . $e->getMessage();
+}
